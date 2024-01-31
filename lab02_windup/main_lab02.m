a0b89073ecbf2d9bbe88077b9b7a3b527fca49ef
YB <137041293+BezkorovainyiYurii@users.noreply.github.com>
2024-01-31 22:46:43 +0200

    update lab02

--- lab02_windup/main_lab02.m
+++ lab02_windup/main_lab02.m
@@ -16,17 +16,25 @@ x0=[0;0;0;0];
 % Моделювання обмеженного PID регулювання за схемою clamping
 [t2,y2]=ode45(@model_clamping,[0:0.1:60],x0);
 
+% Моделювання обмеженного PID регулювання за схемою back-calculation
+[t3,y3]=ode45(@model_backcalculation,[0:0.1:60],x0);
 
 
 figure(1)
 subplot(2,1,1)
-plot(t,y(:,3),t1,y1(:,3),t2,y2(:,3));
+plot(t,y(:,3));
+hold on;
+plot(t1,y1(:,3));
+plot(t2,y2(:,3));
 grid on
 title("Значення регульованого параметру");
 xlabel("Час, с");
 subplot(2,1,2)
-plot(t, y(:,4),t1, y1(:,4),t2, y2(:,4));
-legend("Classic PID","Bound PID","Clamping PID");
+plot(t, y(:,4));
+hold on;
+plot(t1, y1(:,4));
+plot(t2, y2(:,4));
+legend("Classic PID","Bound PID","Clamping PID","Back Calculation PID");
 grid on;
 title("Значення інтегральної складової");
 xlabel("Час, с");
